37e6296eaf8b8b68ba51d72452246289624f927f
joytsing <792566033@qq.com>
2020-04-26 19:27:27 +0800

    update

---
+++ Concurrency_Tools/collections/predecessor/hashtableDemo.java
@@ -0,0 +1,16 @@
+package collections.predecessor;
+
+import java.util.Hashtable;
+
+/**
+ * @description：TODO
+ */
+public class hashtableDemo {
+    public static void main(String[] args) {
+        Hashtable<String,String> hashtable=new Hashtable<>();
+        hashtable.put("aaa","12345");
+        System.out.println(hashtable.get("aaa"));
+    }
+
+
+}
---
+++ Concurrency_Tools/collections/predecessor/vectorDemo.java
@@ -0,0 +1,15 @@
+package collections.predecessor;
+
+import java.util.Vector;
+
+/**
+ * @description：TODO
+ */
+public class vectorDemo {
+
+    public static void main(String[] args) {
+        Vector<String> vector=new Vector<>();
+        vector.add("test");
+        System.out.println(vector.get(0));
+    }
+}
